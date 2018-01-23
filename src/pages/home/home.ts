794a5366ea42ce007798696748efedf88c971d64
Nikhil Thakur <nikhilthakur3197@gmail.com>
2018-01-23 23:12:42 +0530

    ..

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { StudentloginPage } from '../studentlogin/studentlogin';
 
 @Component({
   selector: 'page-home',
@@ -8,7 +9,11 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
+    
+  }
 
+  studentLogin() {
+    this.navCtrl.push(StudentloginPage);
   }
 
 }
