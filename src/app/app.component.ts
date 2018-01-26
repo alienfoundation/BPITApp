c7764ea9cd00e3087eae0bd698fe8ef0f1a9e067
Nikhil Thakur <nikhilthakur3197@gmail.com>
2018-01-26 18:44:02 +0530

    ..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { AttallPage } from '../pages/attall/attall';
+import { CanteenPage } from '../pages/canteen/canteen';
 
 @Component({
   templateUrl: 'app.html'
@@ -22,7 +23,8 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'Atall', component: AttallPage }
+      { title: 'Atall', component: AttallPage },
+      { title: 'Canteen', component: CanteenPage }
     ];
 
 
